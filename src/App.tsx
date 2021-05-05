7ffb814f2df24671cbd5b2b6b46a658ec99e2c2a
Matt Drake <drake.mattc@gmail.com>
2021-05-04 22:04:09 -0400

    Include test data

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,10 @@ import { Navbar } from "./components/Navbar";
 // Types
 import { Album } from "./types";
 
-function App() {
-  const albums: Album[] = [];
-  for (let i = 0; i < 10; ++i) {
-    albums.push({
-      artist: "Opeth",
-      title: "Ghost Reveries",
-      releaseDate: new Date(),
-      coverURL: new URL(
-        "https://upload.wikimedia.org/wikipedia/en/5/52/Ghost_Reveries_%28Opeth%29_album_cover.jpg"
-      ),
-    });
-  }
+// Test data
+import { albums } from "./testData/albums";
 
+function App() {
   return (
     <div>
       <Navbar />
