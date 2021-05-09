43099506bc254a923dd507cc357d14927587a32c
Matt Drake <drake.mattc@gmail.com>
2021-05-09 01:12:07 -0400

    Hack attempt on fakeGenre API

--- src/fakeAPI.ts
+++ src/fakeAPI.ts
@@ -2,13 +2,15 @@ import { Album } from "./types";
 
 const faker = require("faker");
 const albumsMax = 1000;
-const pageSize = 30;
+const pageSize = 10;
+const fakeGenres: string[] = faker.lorem.words(5).split(" ");
 
 // Fake album data
-const albums: Album[] = [];
+let albums: Album[] = [];
 albums.push({
   artist: `${faker.name.firstName()} ${faker.name.lastName()} ${faker.name.lastName()}`,
   title: faker.lorem.words(Math.floor(Math.random() * 10) + 7),
+  genre: fakeGenres[Math.floor(Math.random() * 5)],
   releaseDate: faker.date.future(),
   coverURL: new URL(faker.image.image(200, 200)),
 });
@@ -16,15 +18,48 @@ for (let i = 0; i < albumsMax; ++i) {
   albums.push({
     artist: `${faker.name.firstName()} ${faker.name.lastName()}`,
     title: faker.lorem.words(Math.floor(Math.random() * 5) + 1),
+    genre: fakeGenres[Math.floor(Math.random() * 5)],
     releaseDate: faker.date.future(),
     coverURL: new URL(faker.image.image(200, 200)),
   });
+}
+
+albums = albums.sort((a: Album, b: Album) => {
+  if (a.releaseDate < b.releaseDate) {
+    return -1;
+  } else if (a.releaseDate === b.releaseDate) {
+    return 0;
+  } else {
+    return 1;
+  }
+});
+
+// HACK: sort albums into buckets by genre
+const albumsByGenre: Album[][] = [[], [], [], [], []];
+for (let i = 0; i < 5; ++i) {
+  for (let j = 0; j < albums.length; ++j) {
+    if (albums[j].genre === fakeGenres[j]) {
+      albumsByGenre[i].push(albums[j]);
+    }
+  }
 }
 
 const getAlbums = (page: number): Album[] => {
   const start = page * pageSize;
   const end = start + pageSize;
   return albums.slice(start, end);
+};
+
+const getGenres = (genre: string, page: number): Album[] => {
+  // Find bucket by looking up index in fakeGenres
+  const bucket = fakeGenres.indexOf(genre);
+  if (bucket === -1) {
+    return [];
+  }
+
+  const start = page * pageSize;
+  const end = start + pageSize;
+  return albumsByGenre[bucket].slice(start, end);
 };
 
 exports.getAlbums = getAlbums;
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export interface Album {
   artist: string;
   title: string;
+  genre: string;
   releaseDate: Date;
   coverURL: URL;
 }
