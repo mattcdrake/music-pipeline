fd108652adeb46b59dc00d5bb01e92b323638908
Matt Drake <drake.mattc@gmail.com>
2021-06-09 20:03:52 -0400

    Update dev build dependencies

--- server/src/scraper/scraper.ts
+++ server/src/scraper/scraper.ts
@@ -3,6 +3,7 @@ import { mergeAlbums } from "./merger";
 import { AlbumJSON } from "../../../types/src/types";
 
 const executeArgs = async () => {
+  // Wikipedia scraping routine
   if (process.argv.includes("--wiki")) {
     const albums: AlbumJSON[] = await scrapeWiki();
     mergeAlbums(albums);
