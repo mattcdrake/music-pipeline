dfb687677b112752800f4cadd839116b7dac7e9c
Matt Drake <drake.mattc@gmail.com>
2021-06-16 21:56:03 -0400

    Finish raw monthly table scraper

--- server/src/scraper/wiki.ts
+++ server/src/scraper/wiki.ts
@@ -4,7 +4,7 @@ import cheerio from "cheerio";
 import got from "got";
 
 // Types
-import { AlbumJSON } from "../../../types/src/types";
+import { Album, AlbumJSON } from "../../../types/src/types";
 
 // Constants
 const WIKI_PAGE = "https://en.wikipedia.org/wiki/List_of_2021_albums";
@@ -30,16 +30,58 @@ const getHTML = async (url: string): Promise<string> => {
  *
  * @param {cheerio.Root} $
  * @param {cheerio.Cheerio} row
- * @param {AlbumJSON[]} albums
+ * @returns {AlbumJSON}
  */
-const processAlbum = (
-  $: cheerio.Root,
-  row: cheerio.Element,
-  albums: AlbumJSON[]
-) => {
+const processAlbum = ($: cheerio.Root, row: cheerio.Element): AlbumJSON => {
   const cols = $(row).children().toArray().slice(0, 5);
   const colVals = cols.map((cell) => $(cell).text().trim());
-  console.log(colVals);
+  return {
+    id: "",
+    artist: colVals[1],
+    title: colVals[2],
+    genre: colVals[3],
+    releaseDate: colVals[0],
+    coverURL: "",
+  };
+};
+
+/**
+ * Processes a month table and produces an array of albums.
+ *
+ * @param {cheerio.Root} $
+ * @param {cheerio.Cheerio} rows
+ * @returns {AlbumJSON[]}
+ */
+const processMonthTable = (
+  $: cheerio.Root,
+  rows: cheerio.Cheerio
+): AlbumJSON[] => {
+  // Some rows in the table share a release date. I'm iterating through the
+  // table and tracking which elements have a rowspan value. If it does,
+  // save the date and the number of times it should be used. If the row
+  // _uses_ a prior date value, prepend the element array with the last
+  // date.
+  let albums: AlbumJSON[] = [];
+  let lastDate: cheerio.Cheerio;
+  let lastDateCt = 0;
+
+  for (const row of rows) {
+    const rowspan = $(row).children().first().attr().rowspan;
+
+    if (lastDateCt > 0) {
+      $(lastDate).prependTo($(row));
+      lastDateCt--;
+    }
+
+    if (typeof rowspan !== "undefined") {
+      lastDate = $(row).children().first();
+      lastDateCt = parseInt(rowspan) - 1;
+    }
+
+    albums.push(processAlbum($, row));
+  }
+
+  return albums;
 };
 
 /**
@@ -80,45 +122,13 @@ export const scrapeWiki = async (): Promise<AlbumJSON[]> => {
   const $ = cheerio.load(wikiHTML);
   const wikitables = $(".wikitable tbody").toArray();
 
-  // This needs to be iterated for every table.
-  {
-    // Determine whether this is a month/quarter table
-    const table = wikitables[0];
-    const rows = $("tr", table).slice(1);
+  for (const table of wikitables) {
+    let rows = $("tr", table).slice(1);
     const firstRow = rows.first();
 
     if (isMonthHeader($, firstRow)) {
-      // Process each album in the table
-      let elemRows = rows.slice(1);
-
-      // Some rows in the table share a release date. I'm iterating through the
-      // table and tracking which elements have a rowspan value. If it does,
-      // save the date and the number of times it should be used. If the row
-      // _uses_ a prior date value, prepend the element array with the last
-      // date.
-      let lastDate: cheerio.Cheerio;
-      let dateIter = 0;
-
-      for (const row of elemRows) {
-        const rowspan = $(row).children().first().attr().rowspan;
-
-        if (dateIter > 0) {
-          $(lastDate).prependTo($(row));
-          dateIter--;
-          console.log(dateIter);
-          console.log($(row).text());
-        }
-
-        if (typeof rowspan !== "undefined") {
-          lastDate = $(row).children().first();
-          dateIter = parseInt(rowspan);
-
-          console.log(lastDate.text());
-        }
-
-        //processAlbum($, row, albums);
-        console.log("------------------------------------------------------");
-      }
+      const tableAlbums = processMonthTable($, rows.slice(1));
+      albums = albums.concat(tableAlbums);
     } else {
       console.log("checking for TBA albums");
     }
